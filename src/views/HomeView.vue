83fd9ba56868479310f8a4e32e2dd3d4568825ac
Lukas <lukas@pop-os.localdomain>
2022-04-08 22:09:35 +0200

    zapoceto dodavanje FireBase-a

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,15 +5,22 @@
 <script>
 import { computed } from "@vue/runtime-core";
 import { useStore } from "vuex";
+import { collection, getDocs, getFirestore } from "@firebase/firestore";
+
 export default {
   name: "HomeView",
   setup() {
     const store = useStore();
+    const db = getFirestore();
 
-    const userAccount = computed(() => {
-      return store.state.user;
-    });
-
+    const userAccount = computed(() => store.state.user);
+    const getSnapshot = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().username}`);
+      });
+    };
+    getSnapshot();
     return { userAccount };
   },
 };
